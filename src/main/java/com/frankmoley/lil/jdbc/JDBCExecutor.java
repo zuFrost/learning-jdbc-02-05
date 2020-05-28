91b622012fffe7e1966968db214daa30405793e8
Alex Frost <zufrost.a@gmail.com>
2020-05-29 02:32:23 +0300

    realize Read from DB

--- src/main/java/com/frankmoley/lil/jdbc/JDBCExecutor.java
+++ src/main/java/com/frankmoley/lil/jdbc/JDBCExecutor.java
@@ -13,17 +13,9 @@ public class JDBCExecutor {
         try{
             Connection connection = dcm.getConnection();
             CustomerDAO customerDAO = new CustomerDAO(connection);
-            Customer customer = new Customer();
-            customer.setFirstName("George");
-            customer.setLastName("Washington");
-            customer.setEmail("george.washington@wh.gov");
-            customer.setPhone("(555) 555-6543");
-            customer.setAddress("1234 Main St");
-            customer.setCity("Mount Vernon");
-            customer.setState("VA");
-            customer.setZipCode("22121");
+            Customer customer = customerDAO.findById(1000);
+            System.out.println(customer.getFirstName() + " " + customer.getLastName());
 
-            customerDAO.create(customer);
         }catch(SQLException e){
             e.printStackTrace();
         }
